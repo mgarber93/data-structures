4456625ca818da6ba2ecdaa8b89f5b570db5fefc
Hack Reactor Student <student@hackreactor.com>
2017-07-05 15:40:10 -0700

    Implment queue sharedly

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  instance.storage.size = 0;
+  instance.storage.key = 0;
+  instance.enqueue = queueMethods.enqueue;
+  instance.dequeue = queueMethods.dequeue;
+  instance.size = queueMethods.size;
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  'enqueue': function(value) {
+    this.storage[this.storage.key] = value; 
+    this.storage.size++;
+    this.storage.key++;  
+  },
+  'dequeue': function() {
+    if (this.storage.size <= 0) { return undefined; }
+    var dequeuedEl = this.storage[this.storage.key -
+      this.storage.size];
+    delete this.storage[this.storage.key - this.storage.size];
+    this.storage.size--;
+    return dequeuedEl;
+  },
+  'size': function() {
+    return this.storage['size'];
+  }
+};
 
 
