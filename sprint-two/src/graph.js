afbdd5d5084b5a9b37e0f3bf05153beeee145b1b
Hack Reactor Student <student@hackreactor.com>
2017-07-06 11:42:47 -0700

    Implement Graph addNode, contains, removeNode, hasEdge, addEdge, and forEachNode

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,56 +2,53 @@
 
 // Instantiate a new graph
 var Graph = function() {
-  this.home = null; // a node 
-  this.neighbors = []; // array of graphs
+  this.nodes = [];
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
-  if (this.home === null) { 
-    this.home = node; 
-  } else {
-    let newGraph = new Graph();
-    newGraph.addNode(node);
-    this.neighbors.push(newGraph);
-  }
-  console.log(this.home);
+Graph.prototype.addNode = function(value) {
+  this.nodes.push({'value': value, 'edges': []});
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
-  this.forEachNode(graph => {
-    if (graph.home === node) { return true; } 
-  });
+Graph.prototype.contains = function(value) {
+  for (let node of this.nodes) {
+    if (node.value === value) {
+      return true;
+    } 
+  }
   return false;
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
-  
+Graph.prototype.removeNode = function(value) {
+  this.nodes = this.nodes.filter( node => node.value !== value);
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
-  //return fromNode.neighbors.contains(toNode.home) && toNode.neighbors.contains(fromNode.home);
+Graph.prototype.hasEdge = function(fromValue, toValue) {
+  let matchingNodes = this.nodes.filter( node => node.value === fromValue );
+  if (matchingNodes.length <= 0) { return false; }
+  return matchingNodes[0].edges.filter(edge => edge.value === toValue).length > 0;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {
+Graph.prototype.addEdge = function(fromValue, toValue) {
+  let nodesMatchFrom = this.nodes.filter( node => node.value === fromValue );
+  let nodesMatchTo = this.nodes.filter( node => node.value === toValue );
+  if (nodesMatchTo.length * nodesMatchFrom.length === 0) { return undefined; }
+  nodesMatchFrom[0].edges.push(nodesMatchTo[0]);
+  nodesMatchTo[0].edges.push(nodesMatchFrom[0]);
+  console.log(true);
 };
 
 // Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function(fromNode, toNode) {
+Graph.prototype.removeEdge = function(fromValue, toValue) {
 };
 
 // Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb, history = []) {
-  cb(this.home);
-  history.push(this);
-  let newNeighbors = this.neighbors.filter( e => !history.includes(e)); // array of graphs
-  for (let neighbor of newNeighbors) {
-    if (neighbor !== undefined ) { neighbor.forEachNode(cb, history); }
-  }
+Graph.prototype.forEachNode = function(cb) {
+  this.nodes.map(obj => obj.value).forEach(cb);
 };
 
 /*
