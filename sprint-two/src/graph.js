e2dd44e65be19db5eb397943e7767adc6204c130
interdigitize <kamie.lyn@gmail.com>
2017-07-05 21:28:43 -0700

    Begin to implement graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,22 +2,38 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.home = null; // a node 
+  this.neighbors = []; // array of graphs
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  if (this.home === null) { 
+    this.home = node; 
+  } else {
+    this.neighbors.push(new Graph().addNode(node));
+  }
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (this.home.value === node.value) {
+    return true;
+  }
+  for (let neighbor of this.neighbors) {
+    if (neighbors.contains(node)) { return true; }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return fromNode.neighbors.contains(toNode.home) && toNode.neighbors.contains(fromNode.home);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
