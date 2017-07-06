f5f5cab1827c886bf5b252274f9536bb97b1b97d
Matt <mgarber93@gmail.com>
2017-07-05 23:05:45 -0700

    Implement sets using a object

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,23 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {}; // i'm told that arrays would be cheating
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.storage.hasOwnProperty(JSON.stringify(item))) {
+    this.storage[JSON.stringify(item)] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this.storage.hasOwnProperty(JSON.stringify(item));
 };
 
 setPrototype.remove = function(item) {
+  delete this.storage[JSON.stringify(item)];
 };
 
 /*
