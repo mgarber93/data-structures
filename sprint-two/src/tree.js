c1c03cc21e6a9d850ead9229a3743df524b5df9b
interdigitize <kamie.lyn@gmail.com>
2017-07-05 20:57:19 -0700

    Implement Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,13 +13,22 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) { return true; }
+  for (let child of this.children) {
+    if (child.contains(target)) { return true; }
+  }
+
+  return false;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+addChild: constant
+contains: linear
  */
