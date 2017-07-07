220f5ea3914b1e173bc4cbb6f1cf88d6353897ea
Matt <mgarber93@gmail.com>
2017-07-06 23:03:13 -0700

    Implement naive BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,58 @@
 var BinarySearchTree = function(value) {
+  const bst = Object.create(BinarySearchTree.prototype);
+  bst.value = value;
+  bst.count = 1;
+  bst.left = null;
+  bst.right = null;
+  return bst;
+};
+
+Object.prototype.insert = function (value) {
+  if (this.value < value) {
+    if (this.right !== null) {
+      this.right.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  } else if (this.value > value) {
+    if (this.left !== null) {
+      this.left.insert(value);
+    } else {
+      this.left = BinarySearchTree(value);
+    }
+  } else {
+    this.count++;
+  }
+};
+
+Object.prototype.contains = function (value) {
+  if (this.value === value) {
+    return true;
+  }
+  if (this.left !== null) {
+    if (this.left.contains(value)) { return true; }
+  }
+  if (this.right !== null) {
+    if (this.right.contains(value)) { return true; }
+  }
+  return false;
+};
+
+
+Object.prototype.depthFirstLog = function (cb) {
+  cb(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(cb);
+  }
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert O(logN)
+ * contains O(n)
+ * depthFirstLog O(n)
  */
