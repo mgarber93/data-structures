39b72d7c9efb8102cbb926a233afac935f842217
Hack Reactor Student <student@hackreactor.com>
2017-07-07 16:08:16 -0700

    Implement HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,17 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, k, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index, k);
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.remove(index, k, v);
 };
 
 
--- sprint-two/src/hashTableHelpers.js
+++ sprint-two/src/hashTableHelpers.js
@@ -12,21 +12,38 @@
 //   limitedArray.get(3); // returns 'hi'
 
 var LimitedArray = function(limit) {
-  var storage = [];
-
   var limitedArray = {};
-  limitedArray.get = function(index) {
+  var storage = [];
+  // var size = 0; to track insert count TODO later
+  
+
+  // initalize 
+  for ( var i = 0; i < limit; i++) { 
+     // initalize a new linked list
+    storage[i] = Bucket();
+  }
+
+  limitedArray.get = function(index, key) {
     checkLimit(index);
-    return storage[index];
-  };
-  limitedArray.set = function(index, value) {
+    return storage[index].retrieve(key); 
+  }; 
+  limitedArray.set = function(index, key, value) {
     checkLimit(index);
-    storage[index] = value;
+    // size++; // to track insert count TODO later
+    storage[index].addToTail(key, value);
   };
   limitedArray.each = function(callback) {
-    for (var i = 0; i < storage.length; i++) {
+    for (var i = 0; i < storage.length; i++) { 
+      // this
       callback(storage[i], i, storage);
     }
+  };
+  limitedArray.remove = function(index, key, val) { 
+    // write this
+    // size--;
+    // go to the storage index and call delete pa
+    debugger;
+    storage[index].remove(key, val);
   };
 
   var checkLimit = function(index) {
@@ -52,6 +69,85 @@ var getIndexBelowMaxForKey = function(str, max) {
     hash = Math.abs(hash);
   }
   return hash % max;
+};
+
+var Bucket = function() {
+  var obj = {};
+  obj.head = null;
+  obj.tail = null;
+  obj.contains = BucketMethods.contains;
+  obj.retrieve = BucketMethods.retrieve;
+  obj.addToTail = BucketMethods.addToTail;
+  obj.remove = BucketMethods.remove;
+  return obj;
+};
+
+var BucketMethods = {
+  'remove': function(targetKey, targetVal) {
+    if (this.head === null) {
+      return;
+    }
+    var prev = null;
+    var current = this.head;
+    
+    while (current !== null) {
+      if (current.value[0] === targetKey && 
+        current.value[1] === targetVal) {
+        if (current.value[2] > 1) {
+          current.value--;
+        } else if (prev === null) {
+          this.head = current.next;
+        } else {
+          prev.next = current.next;
+        }
+        break;
+      }
+      // we didn't find target
+      prev = current;
+      current = current.next;
+    }
+  },
+  'retrieve': function(targetKey) {
+    var current = this.head;
+    var lastMatchedKey = undefined; 
+    while (current !== null) {
+      if (current.value[0] === targetKey) {
+        lastMatchedKey = current.value[1]; // value
+      }
+      current = current.next;
+    }
+    return lastMatchedKey;
+  },
+  'contains': function (key) {
+    var current = this.head;
+    while (current !== null) {
+      if (current.value[0] === targetKey) {
+        return true; // value
+      }
+      current = current.next;
+    }
+    return false;
+  },
+  'addToTail': function(key, value) {
+    var newNode = TupNode(key, value);
+    this.head === null ? this.head = newNode : 
+      this.tail.next = newNode;
+    this.tail = newNode;  
+  }
+};
+
+
+var TupNode = function(key, value) {
+  var tupnode = {};
+
+  tupnode.value = [];
+  tupnode.value[0] = key;
+  tupnode.value[1] = value;
+  tupnode.value[2] = 1;
+  tupnode.next = null;
+  tupnode.prev = null; // never used
+
+  return tupnode;
 };
 
 /*
