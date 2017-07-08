f76950f13c0b92928d62e5b42bfebedafbdea318
Hack Reactor Student <student@hackreactor.com>
2017-07-07 20:16:23 -0700

    Add removeChild method

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,34 +1,46 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(Tree.prototype);
   newTree.value = value;
-
-  // your code here
-  newTree.addChild = treeMethods.addChild;
-  newTree.contains = treeMethods.contains;
   newTree.children = [];
-
   return newTree;
 };
 
-var treeMethods = {};
-
-treeMethods.addChild = function(value) {
+Tree.prototype.addChild = function(value) {
   this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+Tree.prototype.removeChild = function(value) {
+  //create storage for children to remove and keep
+  var toRemove = [];
+  var toKeep = [];
+  //iterate through the root's children
+  this.children.forEach( child => {    
+    //if an element's value equals the target, remove array, if not, keep it
+    child.value === value ? toRemove.push(child) : toKeep.push(child);
+  });
+  //get the children from each object in toRemove
+  toRemove.forEach( childToRemove => {
+    if (childToRemove.children) {
+      //keep the children
+      toKeep = toKeep.concat(childToRemove.children);
+    }
+  });
+  //reset children
+  this.children = toKeep;
+};
+
+Tree.prototype.contains = function(target) {
   if (this.value === target) { return true; }
   for (let child of this.children) {
     if (child.contains(target)) { return true; }
   }
-
   return false;
 };
 
 
-
 /*
  * Complexity: What is the time complexity of the above functions?
-addChild: constant
-contains: linear
+ * addChild: constant
+ * contains: linear
+ * removeChild: linear
  */
