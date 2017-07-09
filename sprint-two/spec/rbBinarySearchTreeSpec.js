148b8193a57c04d7a454dddf814b14187ab24b92
Hack Reactor Student <student@hackreactor.com>
2017-07-08 18:36:07 -0700

    Init red black BST with TDD

---
+++ sprint-two/spec/rbBinarySearchTreeSpec.js
@@ -0,0 +1,58 @@
+describe('Red-Black BinarySearchTree', function() {
+  var rbBinarySearchTree;
+
+  beforeEach(function() {
+    rbBinarySearchTree = new RbBinarySearchTree();
+  });
+
+  it('should have methods named "contains", and "depthFirstLog. Nodes should have "insert"', function() {
+    let rbNode = new RBNode(14);
+    expect(rbBinarySearchTree.addChild).to.be.a('function');
+    expect(rbBinarySearchTree.updateNode).to.be.a('function');
+    expect(rbBinarySearchTree.pathLength).to.be.a('function');
+    expect(rbNode.insert).to.be.a('function');
+  });
+
+  it('the root node color should be black', function() {
+    rbBinarySearchTree.addChild(7);
+    expect(rbBinarySearchTree.root.isRed).to.equal(false);
+  });
+
+  it('nodes below the root node color should be red', function() {
+    rbBinarySearchTree.addChild(3); 
+    rbBinarySearchTree.addChild(2);
+    rbBinarySearchTree.addChild(7);
+    expect(rbBinarySearchTree.root.right.isRed).to.equal(true);
+  });
+
+  it('should insert values at the correct location in the tree', function() {
+    rbBinarySearchTree.addChild(5);
+    rbBinarySearchTree.addChild(4);
+    rbBinarySearchTree.addChild(6);
+    rbBinarySearchTree.addChild(3);
+    expect(rbBinarySearchTree.root.value).to.equal(5);
+    expect(rbBinarySearchTree.root.left.value).to.equal(4);
+    expect(rbBinarySearchTree.root.left.isRed).to.equal(false);
+    expect(rbBinarySearchTree.root.left.left.value).to.equal(3);
+    expect(rbBinarySearchTree.root.right.value).to.equal(6);
+    expect(rbBinarySearchTree.root.right.isRed).to.equal(false);
+
+  });
+
+  it('should have a working "contains" method', function() {
+    rbBinarySearchTree.insert(2);
+    rbBinarySearchTree.insert(3);
+    rbBinarySearchTree.insert(7);
+    expect(rbBinarySearchTree.contains(7)).to.equal(true);
+    expect(rbBinarySearchTree.contains(8)).to.equal(false);
+  });
+
+  it('should execute a callback on every value in a tree using "depthFirstLog"', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    rbBinarySearchTree.insert(2);
+    rbBinarySearchTree.insert(3);
+    rbBinarySearchTree.depthFirstLog(func);
+    expect(array).to.eql([5, 2, 3]);
+  });
+});
---
+++ sprint-two/src/rbBinarySearchTree.js
@@ -0,0 +1,119 @@
+class RbBinarySearchTree {
+  constructor() {
+    this.root = null;    
+  }
+
+  addChild(value) {
+    if (this.root === null) {
+      this.root = new RBNode(value, false);
+    } else {
+      //debugger;
+      let path = this.root.insert(value);
+      if (path.length > 2 ) {
+        path.pop();
+        path.push(!path.pop());  //path to uncle 
+        // recoloring
+        // rebalancing
+        if (updateNode(path, n => n.isRed)) {
+          updateNode(path, n => n.isRed = false);
+          path.push(!path.pop());
+          updateNode(path, n => n.isRed = false);
+          updateNode(path.pop(), n => n.isRed = true);
+        } else {
+          let pathToParent = path.slice();
+          let beta = pathToParent.pop(); // done
+
+          let pathToGP = pathToParent.slice();
+          pathToGP.pop();
+          
+          let parent = updateNode(pathToParent, n => n);
+          parent[beta ? 'left' : 'right'] = updateNode(
+            pathToGP,
+            n => n);
+          let alpha = pathToGP.pop(); //now is ggp
+          let ggp = updateNode(pathToGP, n => n);
+          ggp[alpha ? 'right' : 'false'] = parent;
+           
+        }
+        this.root.isRed = false;
+      }
+      
+    }
+  }
+
+  updateNode(path, cb) {
+    let copy = path.slice();
+    let node = this.root;
+    while (copy.length > 0) {
+      if (node === null) {
+        return;
+      }
+      if (copy[0]) {
+        node = node.right;
+      } else {
+        node = node.left;
+      }
+      copy.shift();
+    }
+    if (node !== null) {
+      return cb(node);
+    }
+  }
+
+  pathLength(value) {
+    // return shortest path to value
+  }
+
+  show(nodes = [this.root]) {
+    console.log(nodes.map(n => n.value).join(' '));
+    let next = [];
+    nodes.forEach(n => {
+      if (n.left !== null) {
+        next.push(n.left);
+      } 
+      if (n.right !== null) {
+        next.push(n.right);
+      }
+    });
+    show(nodes);
+  }
+  
+  // rotateCheck() {
+
+  // }
+
+  // rotate() {
+    
+  // }
+  
+}
+
+class RBNode {
+  constructor(value, isRed = true) {
+    this.value = value;
+    this.isRed = isRed;
+    this.left = null;
+    this.right = null;
+  }
+  
+  insert(value) {
+    if (this.value < value) {
+      if (this.right !== null) {
+        return [true].concat(this.right.insert(value)); 
+      } else {
+        this.right = new RBNode(value);
+        return [];
+      }  
+    } else if (this.value > value) {
+      if (this.left !== null) {
+        return [false].concat(this.left.insert(value));
+      } else {
+        this.left = new RBNode(value);
+        return [];
+      }
+    } else {
+      // debugger;
+      return [];
+    }
+  }
+} 
